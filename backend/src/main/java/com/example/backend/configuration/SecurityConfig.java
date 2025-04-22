10e70ca433846720cc648c255f3da3b147327de8
sagiri <thang.nh225763@sis.hust.edu.vn>
2025-04-22 20:38:21 +0700

    feat(notification): add notification service and config kafka

--- backend/src/main/java/com/example/backend/configuration/SecurityConfig.java
+++ backend/src/main/java/com/example/backend/configuration/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.backend.config;
+package com.example.backend.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
--- backend/src/main/java/com/example/backend/controller/UserController.java
+++ backend/src/main/java/com/example/backend/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.example.backend.controller;
 
 import com.example.backend.dto.request.EmailRequest;
+import com.example.backend.event.NotificationEvent;
+import com.example.backend.kafka.NotificationEventProducer;
 import com.example.backend.service.EmailService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +16,9 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 	@Autowired
 	private EmailService emailService;
+	
+	@Autowired
+	private NotificationEventProducer notificationEventProducer;
 
 	@GetMapping
 	@PreAuthorize("hasAnyRole('USER', 'ADMIN')")
@@ -36,5 +42,12 @@ public class UserController {
 				request.isHtml()
 		);
 		return "Email is being sent.";
+	}
+
+	@PostMapping("/notify")
+	public ResponseEntity<String> sendNotification(@RequestBody NotificationEvent notificationEvent) {
+		log.info("Sending notification: {}", notificationEvent);
+		notificationEventProducer.sendNotificationEvent(notificationEvent);
+		return ResponseEntity.ok("Notification sent successfully");
 	}
 }
